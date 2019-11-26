996fc3c6f471c136aa2c95b4301f8c1f7746aaf3
Florian Feyen <florian@feyen.net>
2019-11-26 11:34:03 +0100

    add getHeader function to export headers for requests

--- config.js
+++ config.js
@@ -2,3 +2,12 @@ export const urlSearch = 'http://wp3.x5gon.org/searchengine/v1';
 export const urlRecommender = 'http://wp3.x5gon.org/recommendsystem/v1';
 
 export const modelTypes = ['doc2vec', 'wikifies', 'tfidf'];
+
+export const getHeaders = () => {
+  const headers = {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+  };
+
+  return headers;
+};
