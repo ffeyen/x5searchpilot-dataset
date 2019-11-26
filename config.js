a41a6e3cab999fd8a9159523b67cf1edc36db3cc
Florian Feyen <florian@feyen.net>
2019-11-26 11:25:29 +0100

    add x5gon api configs

---
+++ config.js
@@ -0,0 +1,4 @@
+export const urlSearch = 'http://wp3.x5gon.org/searchengine/v1';
+export const urlRecommender = 'http://wp3.x5gon.org/recommendsystem/v1';
+
+export const modelTypes = ['doc2vec', 'wikifies', 'tfidf'];
