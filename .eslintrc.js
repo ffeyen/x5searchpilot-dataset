bff9f50eb89d32b6ac5b9dda1c86323a9ff7c07f
Florian Feyen <florian@feyen.net>
2019-11-28 16:14:49 +0100

    add eslint rules; max-len 120 and no-param-reassign props: false

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,8 @@ module.exports = {
   },
   rules: {
     "no-console": "off",
-    "no-await-in-loop": "off"
+    "no-await-in-loop": "off",
+    "no-param-reassign": ["warn", { "props": false }],
+    "max-len": ["error", { "code": 120 }]
   }
 };
