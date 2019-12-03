958820d4a2dea61084bb09218db87e5fb301be0c
Florian Feyen <florian@feyen.net>
2019-12-03 16:18:15 +0100

    change conditions and add typeof

--- sort.js
+++ sort.js
@@ -7,21 +7,21 @@ sorter.count = (input) => {
   for (let i = 0; i < workdata.length; i += 1) {
     const { results } = workdata[i].attributes;
 
-    if (aggregatedResults[i] !== 'undefined') {
+    if (typeof aggregatedResults[i] === 'undefined') {
       aggregatedResults[i] = {};
     }
 
     for (let k = 0; k < results.length; k += 1) {
       const materialId = results[k].material_id;
       const modelType = results[k].model_type;
+      const resultWeight = results[k].weight;
 
       if (typeof aggregatedResults[i][materialId] === 'undefined') {
         aggregatedResults[i][materialId] = {};
-        aggregatedResults[i][materialId].modelTypes = [];
       }
 
-      if (!aggregatedResults[i][materialId].modelTypes.includes(modelType)) {
-        aggregatedResults[i][materialId].modelTypes.push(modelType);
+      if (typeof aggregatedResults[i][materialId].modelTypes === 'undefined') {
+        aggregatedResults[i][materialId][modelType] = resultWeight;
       }
     }
   }
